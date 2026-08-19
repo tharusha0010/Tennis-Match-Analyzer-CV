28975bb76e6ac16ac409be0922ec6bf3d9aed69a
Tharusha Ariyarathna <tharushasandaru1111@gmail.com>
2026-08-19 23:45:36 +0530

    test_model.py - add single image test script using Roboflow HTTP client

--- test_model.py
+++ test_model.py
@@ -2,9 +2,9 @@ from inference_sdk import InferenceHTTPClient
 
 CLIENT = InferenceHTTPClient(
     api_url="https://serverless.roboflow.com",
-    api_key="rWJd7YhKrAjVu4Z395W7" 
+    api_key="5tedwy8cTM2JfRId8RHu" 
 )
 
-result = CLIENT.infer(r"D:\Projects\Tennis-Match-Analyzer-CV\extracted_frames\frame_0046.jpg", model_id="tennis-match-analyzer-cv/2")
+result = CLIENT.infer(r"D:\Projects\Tennis-Match-Analyzer-CV\extracted_frames\frame_0046.jpg", model_id="tennis-match-analyzer-cv-bbwo3/1")
 
 print(result)
